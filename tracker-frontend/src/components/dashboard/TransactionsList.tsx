7ef6745c38a7046ce35af9223fe62130d29c8729
Satyam <satyamagrawalread@gmail.com>
2024-10-20 14:59:31 +0530

    Fixed skeleton issue

--- tracker-frontend/src/components/dashboard/TransactionsList.tsx
+++ tracker-frontend/src/components/dashboard/TransactionsList.tsx
@@ -167,8 +167,8 @@ const TransactionsList = () => {
               <div key={index} className="flex items-center space-x-4">
                 <Skeleton className="h-12 w-12 rounded-full" />
                 <div className="space-y-2">
-                  <Skeleton className="h-4 w-[250px]" />
-                  <Skeleton className="h-4 w-[200px]" />
+                  <Skeleton className="h-4 sm:w-[250px] w-[180px]" />
+                  <Skeleton className="h-4 xs:w-[200px] w-[130px]" />
                 </div>
               </div>
             ))}
